a78d63a3cec190c09f5fb8f55e50840f781605fe
anthonypse <anthonypse@gmail.com>
2018-11-25 23:22:07 -0500

    Now passes the smoke test.  i.e. doesn't explode on startup.

--- app/src/main/java/com/anthonypse/androidwidgetsdemo/gui/DemoWidget.java
+++ app/src/main/java/com/anthonypse/androidwidgetsdemo/gui/DemoWidget.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.anthonypse.androidwidgetsdemo.InfoUtil;
 import com.anthonypse.androidwidgetsdemo.R;
