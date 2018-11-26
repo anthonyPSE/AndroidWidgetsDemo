1f089733718c52a8c351b2229b6ab46d0031112e
anthonypse <anthonypse@gmail.com>
2018-11-25 23:55:26 -0500

    Adding permissions and functionality for bluetooth.

--- androidutils/src/main/java/com/anthonypse/androidutils/NetworkManager.java
+++ androidutils/src/main/java/com/anthonypse/androidutils/NetworkManager.java
@@ -1,28 +1,62 @@
 package com.anthonypse.androidutils;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.provider.Settings;
 
 public class NetworkManager {
     private static final String TAG = "NetworkManager";
-    private Context mContext;
+    private Context mApplicationContext;
 
     public NetworkManager(Context context) {
-        mContext = context;
+        mApplicationContext = context;
 
     }
 
     public boolean isWifiEnabled(){
+        WifiManager manager = (WifiManager) mApplicationContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (manager != null) {
+            return manager.isWifiEnabled();
+        }
         return false;
     }
     public boolean isBlueToothEnabled(){
-        return false;
+        if( BluetoothAdapter.getDefaultAdapter() != null ){
+            return BluetoothAdapter.getDefaultAdapter().isEnabled();
+        } else {
+            throw new NullPointerException();
+        }
     }
 
-    public boolean setWifiEnabled(boolean enabled){
-        return false;
+    public boolean setWifiEnabled(boolean isEnabled){
+        WifiManager manager = (WifiManager) mApplicationContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        try {
+            if(manager == null)
+                return false;
+            return manager.setWifiEnabled(isEnabled);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    public boolean setBluetoothEnabled(boolean enabled){
-        return false;
+    public boolean setBluetoothEnabled(boolean isEnabled){
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        try {
+            if (isEnabled) {
+                return adapter.enable();
+            } else {
+                return adapter.disable();
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean isAirplaneModeEnabled() {
+        return Settings.System.getInt(mApplicationContext.getContentResolver(),
+                Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
     }
 }
